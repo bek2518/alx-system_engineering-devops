f183b583977f7e674c89a56f0efcfb4915775824
bek2518 <bereketb20@hotmail.com>
2023-01-11 09:20:05 +0300

    added name

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Create a file in /tmp using Puppet
-file {:
-    path    => '/tmp/school',
+file { '/tmp/school':
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet'
+    content => 'I love Puppet',
 }
