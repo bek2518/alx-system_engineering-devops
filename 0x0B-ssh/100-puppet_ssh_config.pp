8f110ab5447d6f0db496de27bef1ea3a4873043b
bek2518 <bereketb20@hotmail.com>
2023-01-11 18:23:33 +0300

    Trial

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file { '/etc/ssh/ssh_config':
   ensure => present
-}
+}=>
 file_line { 'Append a line to /etc/ssh/ssh_config':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school\nPasswordAuthentication no'
