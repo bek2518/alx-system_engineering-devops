422939ffab8e2bec95e25172601972a8defc006e
bek2518 <bereketb20@hotmail.com>
2023-01-13 10:57:01 +0300

    Puppet file to configure web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,28 @@
+# Installs Nginx server and configures redirect
+
+exec { 'installation':
+  provider => shell,
+  command  => 'sudo apy -y update ; sudo apt -y install nginx ; sudo echo 
+  "Hello World!" > /var/www/html/index.html'
+}
+
+file { 'configuration':
+  ensure  => present,
+  content => 'server {
+        listen 80;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+        if (\$request_filename ~ redirect_me){
+                rewrite ^ https://www.youtube.com/watch?v=IotUxgPqbik permanent;
+        }'
+}
+
+exec { 'start service':
+  provider => shell,
+  command  => sudo service nginx start
+}
--- 0x0C-web_server/README.md
+++ 0x0C-web_server/README.md
@@ -17,3 +17,4 @@ No | File | Description
 3 | 2-setup_a_domain_name | Setup a .tech domain name
 4 | 3-redirection | Configures a new server and redirects to another page
 5 | 4-not_found_page_404 | Based on the redirect and adds custom 404 page
+6 | 7-_puppet_install_nginx_web_server.pp | Puppet file to install and configure Nginx web server
