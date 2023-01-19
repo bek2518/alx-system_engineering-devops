089aac616de0a6c48906467975661f135bc840f5
bek2518 <bereketb20@hotmail.com>
2023-01-19 19:19:48 +0300

    Reverting bacl

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,16 @@
 # Installs Nginx server and configures redirect
 
 exec { 'installation':
-  command  => 'sudo apt -y update ; sudo apt -y install nginx',
+  command  => 'sudo apt -y update ; sudo apt -y install nginx ; echo 
+  "Hello World!" > /var/www/html/index.html ; sudo service nginx start',
   provider => shell,
 }
 
-file { '/var/www/html/index.html':
+file { 'Configuration':
   ensure  => file,
-  content => 'Hello World!',
-}
-
-exec { 'start nginx':
-  provider => shell
-  command  => 'sudo service nginx start',
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => 'server {
+  path    => '/etc/nginx/sites-enabled/default',
+  content => 
+  'server {
         listen 80 default_server;
         listen [::]:80 default_server;
         root /var/www/html;
@@ -32,7 +25,9 @@ file { '/etc/nginx/sites-enabled/default':
     }'
 }
 
+
+
 exec { 'restart service':
   provider => shell,
-  command  => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart'
   }
