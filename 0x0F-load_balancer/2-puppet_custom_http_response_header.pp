ec5a9bae69e964da71273c7d30c90ce6615ddec8
bek2518 <bereketb20@hotmail.com>
2023-01-19 19:51:37 +0300

    Another typo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Automates the task of creating a custom HTTP response header response
 # using puppet
 
-exec {'Install Configure':
+exec { 'Install Configure':
   provider => shell,
-  command   => 'sudo apt -y update ; sudo apt -y install nginx" ;
+  command   => 'sudo apt -y update ; sudo apt -y install nginx ;
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
   service nginx restart',
 }
