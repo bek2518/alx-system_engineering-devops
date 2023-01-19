88fb5c01b57df3929611efc5c0a7d33e0153778e
bek2518 <bereketb20@hotmail.com>
2023-01-19 19:55:35 +0300

    Trial

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,7 +3,7 @@
 
 exec { 'Install Configure':
   provider => shell,
-  command   => 'sudo apt -y update ; sudo apt -y install nginx ;
+  command   => 'sudo apt -y update ; sudo apt -y install nginx ; sudo service nginx start
   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
   service nginx restart',
 }
