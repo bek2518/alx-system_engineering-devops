bcb5706fa0ed624bcc184731504b9c40bdbd3fe6
bek2518 <bereketb20@hotmail.com>
2023-01-19 19:44:12 +0300

    Replaced with sections

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,29 @@
 # Automates the task of creating a custom HTTP response header response
 # using puppet
 
-exec { 'Install Configure':
+exec { 'installation':
+  command  => 'sudo apt -y update ; sudo apt -y install nginx ; sudo service nginx start',
   provider => shell,
-  command  => "sudo apt -y update && sudo apt -y install nginx &&
-  sudo sed -i '/listen 80 default_server;/a add_header X-Served-By
-  $hostname;' /etc/nginx/sites-available/default && service nginx restart",
 }
+
+file { 'Configuration':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+  'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                add_header X-Served-By $hostname;
+                try_files $uri $uri/ =404;
+        }
+    }'
+}
+
+exec { 'restart service':
+  provider => shell,
+  command  => 'sudo service nginx restart'
+  }
