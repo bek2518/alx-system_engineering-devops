6d101fca43c8b32e1a837f4674a61c8bcfb20ecb
bek2518 <bereketb20@hotmail.com>
2023-01-22 10:15:31 +0300

    Escape character for hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,6 +4,6 @@
 exec { 'Install Configure':
   provider => shell,
   command  => 'sudo apt -y update ; sudo apt -y install nginx ; sudo service nginx start ;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default;
   service nginx restart',
 }
