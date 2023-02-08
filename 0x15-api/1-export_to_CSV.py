f63488ab0fe6779ec53ae61e67c9cbaed9388dce
bek2518 <bereketb20@hotmail.com>
2023-02-08 15:13:57 +0300

    Rename and pycodestyle edit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that exports data in CSV format
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+    employee_id = argv[1]
+
+    user_response = requests.get(f"{url}/users/{employee_id}")
+    response = requests.get(f"{url}/users/{employee_id}/todos")
+    name = user_response.json()["name"]
+    tasks = [task for task in response.json()]
+
+    with open('{}.csv'.format(employee_id), 'w') as f:
+        for task in tasks:
+            completed = task['completed']
+            title = task['title']
+            f.write(
+                f'"{employee_id}","{name}","{completed}",{title}\n'
+            )
