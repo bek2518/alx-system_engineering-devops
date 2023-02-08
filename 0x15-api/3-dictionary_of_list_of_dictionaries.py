bf0519a17cc705979eee6cbc2aa303df7acd69af
bek2518 <bereketb20@hotmail.com>
2023-02-08 17:31:49 +0300

    Exports all data to json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Python script that exports data in JSON format
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+
+    user_response = requests.get(f"{url}/users/").json()
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump({user.get("id"): [{
+            "username": user.get("username"),
+            "task": task.get("title"),
+            "completed": task.get("completed")}
+            for task in requests.get("{}/users/{}/todos".
+                                     format(url, user.get("id"))).json()]
+            for user in user_response}, f)
