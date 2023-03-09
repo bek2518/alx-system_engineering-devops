9a172581f691ac0a1724532363ccc6b68e58324d
bek2518 <bereketb20@hotmail.com>
2023-03-09 20:01:09 +0300

    Removed sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,10 +2,10 @@
 
 exec {'Edit nginx default file':
   provider => shell,
-  command  => 'sudo sed -i "s5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx'
+  command  => 'sed -i "s5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx'
 }
 
 exec {'restart nginx':
   provider => shell,
-  command  => 'sudo service nginx restart'
+  command  => 'service nginx restart'
 }
